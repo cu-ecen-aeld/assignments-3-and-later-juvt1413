c4c9111105f98f70f5680d51a8ba3ef8e87b2c93
juvt1413 <m.aldossari210@gmail.com>
2022-10-25 22:25:42 +0200

    Assignment-4-part-1_1st_commit

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -2,6 +2,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 // Optional: use these functions to add debug or error prints to your application
 #define DEBUG_LOG(msg,...)
@@ -13,7 +14,25 @@ void* threadfunc(void* thread_param)
 
     // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
     // hint: use a cast like the one below to obtain thread arguments from your parameter
-    //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    
+    usleep(thread_func_args->wait_to_obtain_ms);
+    
+    int lock = pthread_mutex_lock(thread_func_args->mutex);
+    if(lock != 0){
+    	thread_func_args->thread_complete_success = false;
+    	printf("Mutex Lock Failed\n");
+    }
+    else{
+    	usleep(thread_func_args->wait_to_release_ms);
+    	int unlock = pthread_mutex_unlock(thread_func_args->mutex);
+    	if(unlock != 0){
+    	    thread_func_args->thread_complete_success = false;
+    	    printf("Mutex Release Failed\n");
+    	} 
+    }
+    thread_func_args->thread_complete_success = true;    
+    
     return thread_param;
 }
 
@@ -28,6 +47,23 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
      *
      * See implementation details in threading.h file comment block
      */
-    return false;
+    struct thread_data *thread_struct = malloc(sizeof(struct thread_data));
+    memset(thread_struct,0,sizeof(struct thread_data));
+    thread_struct->mutex = mutex;
+    thread_struct->wait_to_obtain_ms = wait_to_obtain_ms;
+    thread_struct->wait_to_release_ms = wait_to_release_ms;
+    
+    int rc = pthread_create(thread,NULL,threadfunc,(void *)thread_struct);
+    
+    if(rc != 0){
+    	printf("Unable to create the thread successfully");
+    	thread_struct->thread_complete_success = false;
+    }
+    else{
+    	thread_struct->thread_complete_success = true;
+    }
+    
+    return thread_struct->thread_complete_success;    
+     
 }
 
